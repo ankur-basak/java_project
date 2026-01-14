bb43961431b5996ce246f0ddb67390e533031443
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2026-01-14 21:37:35 +0600

    Project start file

---
+++ Passenger Registration/Start.java
@@ -0,0 +1,8 @@
+import Frame.PassengerRegistration;
+
+public class Start {
+	public static void main(String[] args){
+        PassengerRegistration obj = new PassengerRegistration();
+        obj.setVisible(true);
+    }
+}
