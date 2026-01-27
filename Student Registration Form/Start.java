6188b4a8fa744dfabdfaa35173e61025e144a10b
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2026-01-27 20:07:24 +0600

    Add files via upload

---
+++ Student Registration Form/Start.java
@@ -0,0 +1,8 @@
+import Frame.StudentRegistration;
+
+public class Start {
+	public static void main(String[] args){
+        StudentRegistration obj = new StudentRegistration();
+        obj.setVisible(true);
+    }
+}
